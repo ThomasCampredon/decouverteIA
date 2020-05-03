0b2436c39ad2987df49200c2a048884dbe3ee84d
ThomasCampredon <thomas.campredon@etu.unilim.fr>
2020-05-03 16:03:54 +0200

    ajout des commentaires

--- apprentissage/src/apprentissage/Generateur.java
+++ apprentissage/src/apprentissage/Generateur.java
@@ -5,24 +5,33 @@ import java.util.Collections;
 
 public class Generateur {
 
+	// méthode qui génère tous les binaire de longueur longueur
 	public static ArrayList<ArrayList<Integer>> genererBinaire (int longueur) {
 		ArrayList<ArrayList<Integer>> liste = new ArrayList<>();
-		int nombreDeValeur = (int) Math.pow(longueur,2);
+		int nombreDeValeur = (int) Math.pow(2, longueur); //calcul du nombre de valeurs différentes
 		
+		//pour chaque valeur de 0 au nombre de valeur possible
 		for (int i = 0 ; i < nombreDeValeur ; i++) {
-			ArrayList<Integer> binaire = new ArrayList<>();
+			ArrayList<Integer> binaire = new ArrayList<>(); //liste qui contiendra le binaire
 			int nombre = i;
 			int reste = 0;
+			
+			// on définit chaque bit
 			do {
 				reste = nombre % 2;
 				binaire.add(reste);
 				nombre = (nombre - reste)/2;
 			}while (nombre!=0);
 			
+			// on complete avec des zeros tant que notre binaire n'a pas la taille souhaitée
 			while (binaire.size()<longueur) {
 				binaire.add(0);
 			}
+			
+			// on inverse le binaire pour le mettre dans le bon sens
 			Collections.reverse(binaire);
+			
+			// on l'ajoute à la liste finale
 			liste.add(binaire);
 		}
 		
--- apprentissage/src/apprentissage/Main.java
+++ apprentissage/src/apprentissage/Main.java
@@ -3,8 +3,8 @@ package apprentissage;
 import java.util.ArrayList;
 
 public class Main {
-	static int nombreDeBit = 4;
-	static ArrayList <ArrayList<Integer>> motifs = Generateur.genererBinaire(nombreDeBit);
+	static int nombreDeBit = 4; //on choisit la longueur des entrées
+	static ArrayList <ArrayList<Integer>> motifs = Generateur.genererBinaire(nombreDeBit); //on génère les motifs 
 	
 	public static void main(String[] args) {
 		Neurone neurone = new Neurone(2);
@@ -28,7 +28,7 @@ public class Main {
 			
 			while (motifs.size()>idMotif) {
 				ArrayList<Integer> motifCourant = motifs.get(idMotif);
-				int attendu = 0;
+				int attendu = 0; //on initialise la valeur que le neurone doit renvoyé
 				
 				// on choisi le modèle qui devra donner 1
 				if ((motifCourant.get(0) == 1) && (motifCourant.get(3)==1) && (motifCourant.get(1)==0) && (motifCourant.get(2)==0)){ 
@@ -36,30 +36,39 @@ public class Main {
 				}
 				
 				boolean result = neurone.apprendre(motifCourant, attendu); //indique le resultat du neurone
+				
+				// on teste si un result est faux, si c'est le cas, alors on continue à itérer
 				if (result == false) {
 					end = false;
 				}
 				
+				
 				int z = 0;
+				
+				// on n'a pas accès à la valeur de sortie de la neurone, donc on la déduit
 				if ((attendu==1)&&(result==true)){
 					z = 1;
 				}
 				
+				// on transmet les valeurs à afficher
 				afficherInfo(motifCourant,neurone, iteration, idMotif, attendu, z);
+				
+				// on incrémente l'identifiant du motif à tester
 				idMotif+=1;
 			}
 			System.out.println("\n");
 		}
 	}
 	
+	// méthode qui affiche les résultats
 	public static void afficherInfo(ArrayList<Integer>motif, Neurone neurone, int iteration, int idMotif, int d, int z) {
 		ArrayList <Double> poids = (ArrayList<Double>) neurone.getPoids();
 		String info="";
 		
 		if (z==d) {
-			info+="TRUE ";
-		}else {
 			info+="     ";
+		}else {
+			info+="---> ";
 		}
 		
 		 info += "E" + iteration + "." + (idMotif+1) + " : " + motif.toString() + " : z= " + z + "  d= " + d
